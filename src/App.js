a360b482d9d9386ca6b0ff7246b26effb86b30b0
francescofeston <francescofeston@gmail.com>
2020-09-07 13:31:15 +0200

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class App extends Component{
-  render(){
+class App extends Component {
+  render() {
     return (
       <div>
-          <div className="Header">
-            Phone Directory
+        {/* <div className="Header">
+          Phone Directory
           </div>
-          <button>Add</button>
-          <div>
-            <span>Name</span><br/>
-            <span>Phone</span>
-          </div>
+        <button>Add</button>
+        <div>
+          <span>Name</span><br/>
+          <span>Phone</spa
+    </div>*/}
+    <label htmlFor="name">Name: </label>
+    <input id="name" type="text" placeholder="Type here" defaultValue="franz"/>
       </div>
     );
   }
