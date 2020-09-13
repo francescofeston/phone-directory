3cd81d3a6184be9a070b96f79babc67a89bf2f77
Francesco Feston <francescofeston@gmail.com>
2020-09-13 19:22:30 +0200

    added Header.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <div className="Header">
-          Phone Directory
-          </div>
+         <Header/>
         <button>Add</button>
         <div>
           <span>Name</span><br/>
-          <span>Phone</spa
-    </div>*/}
-    <label htmlFor="name">Name: </label>
-    <input id="name" type="text" placeholder="Type here" defaultValue="franz"/>
-      </div>
+          <span>Phone</span>
+        </div>
+
+        {/*<label htmlFor="name">Name: </label>
+        <input id="name" type="text" placeholder="Type here" defaultValue="franz"/>*/}
+    </div>
     );
   }
 }
---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = function(){
+    return(
+        <div className="Header">
+          Phone Directory
+        </div>
+    )
+}
+
+//class Header extends Component{
+//    render(){
+//        return(
+//            <div className="Header">
+//                 Phone Directory
+//            </div>
+//        )
+//    }
+//}
+export default Header;
